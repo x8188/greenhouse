fcf1e2ef47e41673f7cbf82c7e11bbc3a54eedbb
x8188 <1341002157@qq.com>
2023-08-18 09:50:50 +0800

    home page

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,285 +1,369 @@
 <template>
-  <div class="home_container">
-    <div class="container-fuild">
-      <!-- 导航 -->
-      <div class="header-nav">
-        <!-- 导航logo -->
-        <div class="header-nav-logo">
-          <img src="@/assets/img/hzauLogo.png" />
+  <div>
+    <div class="app">
+      <div class="weather-monitoring">
+        <weather-monitor></weather-monitor>
+      </div>
+      <div class="video">
+        <video-page></video-page>
+      </div>
+      <div class="nutrient-monitoring">
+        <nutrient-monitor></nutrient-monitor>
+      </div>
+    </div>
+
+    <div class="someInfo">
+      <div class="Info"></div>
+      <div class="Info"></div>
+      <div class="Info"></div>
+      <div class="Info"></div>
+    </div>
+
+    <div class="someInfo" style="margin-top: 50px;">
+      <button class="button">
+        Fancy Button
+        <div class="button__horizontal"></div>
+        <div class="button__vertical"></div>
+      </button>
+      <button class="button">
+        Fancy Button
+        <div class="button__horizontal"></div>
+        <div class="button__vertical"></div>
+      </button>
+      <button class="button">
+        Fancy Button
+        <div class="button__horizontal"></div>
+        <div class="button__vertical"></div>
+      </button>
+      <button class="button">
+        Fancy Button
+        <div class="button__horizontal"></div>
+        <div class="button__vertical"></div>
+      </button>
+    </div>
+
+    
+    <!-- <div class="box">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <div class="content">
+          <h2>My animated Border </h2>
+          <p><a>All our modules are designed to play nicely with responsive of course. At the end of the day it comes down to the theme you use - our code doesn't get in your way.</a></p>
         </div>
-      </div>
-      <!-- banner -->
-      <div class="article_banner">
-        <img src="@/assets/img/banner/banner1.jpg" alt="banner" />
-      </div>
-      <!-- 科研项目 -->
-      <div class="container-stage" style="margin-top: 30px;">
-        <div class="stage-row">
-          <div class="stage-col" v-for="(item, index) in typeList" :key="index">
-            <div class="col-container">
-              <img :src="item.logo" alt=""/>
-              <div class="overlay">
-                <span>{{ item.title }}</span>
-                <h4>
-                  <a href="">{{ item.titleDescription }}</a>
-                </h4>
-              </div>
-            </div>
-           <!--  <div>
-              <p>{{ item.description }}</p>
-            </div> -->
-          </div>
-        </div>
-      </div>
+        
+    </div> -->
+
+    <div class="monitor-platform">
+      <monitor-platform></monitor-platform>
     </div>
   </div>
 </template>
 
-<script setup name="Index">
-import useUserStore from "@/store/modules/user";
-import type1 from "@/assets/img/1.jpg"
-import type2 from "@/assets/img/2.jpg"
-import type3 from "@/assets/img/3.jpg"
-const user = useUserStore();
-const version = ref("3.8.2");
+<script >
+import { reactive, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import weatherMonitor from "@/views/sensor/monitor_page/weatherMonitor.vue";
+import nutrientMonitor from "@/views/sensor/monitor_page/nutrientMonitor.vue";
+import videoPage from "@/views/sensor/monitor_page/videoPage.vue";
+import monitorPlatform from "@/views/sensor/monitorplatform/monitorPlatform.vue";
+// import {ref} from 'vue'
+// import { onMounted, onUnmounted } from 'vue'
+import * as echarts from "echarts";
+//
+export default {
+  components: {
+    weatherMonitor,
+    nutrientMonitor,
+    videoPage,
+    monitorPlatform,
+  },
 
-function goTarget(url) {
-  window.open(url, "__blank");
-}
 
-const typeList = reactive([
-  {
-    logo: type1,
-    title: "表型文件",
-    description:"这是作物表型管理平台的表型管理部分"
-  },
-  {
-    logo: type2,
-    title: "数据转换",
-    description:"这是作物表型管理平台的图片管理部分"
-  },
-  {
-    logo: type3,
-    title: "用户管理",
-    description:"这是作物表型管理平台的图片自动上传部分"
-  },
-  {
-    logo: type2,
-    title: "角色管理",
-    description:"这是作物表型管理平台的图片自动上传部分"
-  },
-])
+  // setup() {
+  //   const displayData = ref(null) //存储大屏展示数据的响应式变量
 
+  // },
+  // 从后端 API 获取传感器数据
+  // const fetchSensorData = () => {
+  //   // 使用适当的 API 请求库（如 axios）从后端获取数据
+  //   // 更新 nutrientSensors 和 weatherSensors 中的传感器数值
+  // };
+
+  // onMounted(() => {
+  //   fetchSensorData(); // 在组件挂载后获取传感器数据
+  // });
+};
 
 </script>
 
-<style scoped lang="less">
-.home_container {
-  width: 100%;
-}
-
-.container-fuild {
-  position: relative;
-}
-
-.container-fuild::after {
-  position: absolute;
-  left: 0;
-  bottom: -40px;
-  content: "";
-  height: 50px;
-  width: 100%;
-  background: url(@/assets/img/brush-down.png);
-  z-index: 2;
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: bottom;
-  box-sizing: border-box;
-}
-
-
-/* 顶部 */
-.container-fuild {
-  background: #fff;
-  transition: all ease 0.6s;
-  width: 100%;
-  height: 80px;
-  left: 0;
-  top: 0;
-}
-
-/* 导航栏 */
-.container-fuild .header-nav {
-  height: 110px;
+<style lang='scss' scoped>
+.app {
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
+  align-items: flex-start;
+  /* background-color: #7fb7a191; */
 }
 
-/* 导航栏logo */
-.container-fuild .header-nav .header-nav-logo {
-  width: 400px;
-  height: 100%;
-  float: left;
-  position: relative;
+.nutrient-monitoring,
+.weather-monitoring {
+  /* flex: 1 1 30%;
+  justify-content: center;
+  margin-bottom: 20px;
+  width: 35%;
+  padding: 0 20px; */
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 30%;
+}
+.video {
+  /* flex: 1 1 40%; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40%;
+  /* margin-bottom: 20px;
+  padding: 0 20px; */
+}
+/* .nutrient-monitoring{
+  
+} */
+.sensor-card {
+  background-color: #f2f2f2;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+.someInfo {
+  display: flex;
+  justify-content: space-around;
+}
+.Info {
+  height: 300px;
+  width: 300px;
+
+  padding: 20px;
+
+  background: linear-gradient(
+    to right bottom,
+    rgba(255, 255, 255, 0.7),
+    rgba(255, 255, 255, 0.5),
+    rgba(255, 255, 255, 0.4)
+  );
+  background-color: #ececec;
+
+  /* 使背景模糊化 */
+  backdrop-filter: blur(10px);
+  box-shadow: 0 0 20px #3c9f64;
+  border-radius: 15px;
 }
 
-/* 导航栏logo图片 */
-.container-fuild .header-nav .header-nav-logo img {
-  height: 65px;
+body {
+	
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	background: #fcf3ec;
+	
+}
+
+.button {
+	
+	--offset: 10px;
+	--border-size: 2px;
+	
+	display: block;
+	position: relative;
+	padding: 1.5em 3em;
+	appearance: none;
+	border: 0;
+	background: transparent;
+	color: #3c9f64;
+	text-transform: uppercase;
+	letter-spacing: .25em;
+	outline: none;
+	cursor: pointer;
+	font-weight: bold;
+	border-radius: 0;
+	box-shadow: inset 0 0 0 var(--border-size) currentcolor;
+	transition: background .8s ease;
+	
+	&:hover {
+		background: rgba(100, 0, 0, .03);
+	}
+	
+	&__horizontal,
+	&__vertical {
+		position: absolute;
+		top: var(--horizontal-offset, 0);
+		right: var(--vertical-offset, 0);
+		bottom: var(--horizontal-offset, 0);
+		left: var(--vertical-offset, 0);
+		transition: transform .8s ease;
+		will-change: transform;
+		
+		&::before {
+			content: '';
+			position: absolute;
+			border: inherit;
+		}
+	}
+	
+	&__horizontal {
+		--vertical-offset: calc(var(--offset) * -1);
+		border-top: var(--border-size) solid currentcolor;
+		border-bottom: var(--border-size) solid currentcolor;
+		
+		&::before {
+			top: calc(var(--vertical-offset) - var(--border-size));
+			bottom: calc(var(--vertical-offset) - var(--border-size));
+			left: calc(var(--vertical-offset) * -1);
+			right: calc(var(--vertical-offset) * -1);
+		}
+	}
+	
+	&:hover &__horizontal {
+		transform: scaleX(0);
+	}
+	
+	&__vertical {
+		--horizontal-offset: calc(var(--offset) * -1);
+		border-left: var(--border-size) solid currentcolor;
+		border-right: var(--border-size) solid currentcolor;
+		
+		&::before {
+			top: calc(var(--horizontal-offset) * -1);
+			bottom: calc(var(--horizontal-offset) * -1);
+			left: calc(var(--horizontal-offset) - var(--border-size));
+			right: calc(var(--horizontal-offset) - var(--border-size));
+		}
+	}
+	
+	&:hover &__vertical {
+		transform: scaleY(0);
+	}
+	
+}
+
+
+.box{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  height: 300px;
+  background: #114525a6;
+  box-sizing: border-box;
+  overflow: hidden;
+  box-shadow: 0 20px 50px #3c9f64;
+  border: 2px solid #3c9f64;
+  color: white;
+  padding: 20px;
+}
+ 
+.box:before{
+  content: '';
+  position:absolute;
+  top:0;
+  left:-100%;
+  width:100%;
+  height:100%;
+  background: rgba(255,255,255,0.1);
+  transition:0.5s;
+  pointer-events: none;
+}
+ 
+.box:hover:before{
+  left:-50%;
+  transform: skewX(-5deg);
+}
+ 
+ 
+.box .content{
+  position:absolute;
+  top:15px;
+  left:15px;
+  right:15px;
+  bottom:15px;
+  border:1px solid #f0a591;
+  padding:20px;
+  text-align:center;
+  box-shadow: 0 5px 10px rgba(9,0,0,0.5);
+  
+}
+ 
+.box span{
   position: absolute;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
-  margin: auto;
-}
-
-.article_banner {
   width: 100%;
-  height: 22vh;
-  overflow: hidden;
-}
-.article_banner img {
-  width: 100vw;
   height: 100%;
-  object-fit: cover;
-}
-
-
-/* 为什么选择我们 */
-.container {
-  width: 1170px;
-  margin: auto;
-}
-
-.text-center {
-  text-align: center;
-  margin-top: 30px;
-}
-
-.server-wrapper {
-  margin: 10px;
-}
-
-.center-block {
   display: block;
-  margin-right: auto;
-  margin-left: auto;
+  box-sizing: border-box;
+  
 }
-
-.row {
-  margin-right: -15px;
-  margin-left: -15px;
-  display: flex;
-  color: #1f4e3d;
-  border-color: #1f4e3d;
+ 
+.box span:nth-child(1)
+{
+  transform:rotate(0deg);
 }
-
-.col-md-3 {
-  width: 25%;
+ 
+.box span:nth-child(2)
+{
+  transform:rotate(90deg);
 }
-
-/* 科研项目 */
-.container-stage {
-  width: auto;
-  min-width: auto;
-  max-width: inherit;
-  padding-right: 0;
-  .stage-row {
-    --bs-gutter-x: 30px;
-    --bs-gutter-y: 0;
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: calc(-1 * var(--bs-gutter-y));
-    margin-right: calc(-0.5 * var(--bs-gutter-x));
-    margin-left: calc(-0.5 * var(--bs-gutter-x));
-    justify-content: center !important;
-
-    .stage-col {
-      flex: 0 0 auto;
-      width: 23%;
-      max-width: 90%;
-      padding-right: calc(var(--bs-gutter-x) * 0.5);
-      padding-left: calc(var(--bs-gutter-x) * 0.5);
-      margin-top: var(--bs-gutter-y);
-
-      .col-container {
-        position: relative;
-        padding-bottom: 30px;
-        overflow: hidden;
-        cursor: pointer;
-        display: flex;
-        align-items: flex-end;
-
-        img {
-          border: none;
-          outline: none;
-          max-width: 100%;
-          height: 300px;
-          vertical-align: middle;
-        }
-
-        .overlay {
-          background: #1f4e3d;
-          display: inline-block;
-          position: absolute;
-          z-index: 1;
-          bottom: -10px;
-          right: 16%;
-          padding: 40px 80px;
-          min-width: 65%;
-          height: 100px;
-          transition: height 0.3s;
-          box-sizing: border-box;
-          //visibility: hidden;
-          //opacity: 0;
-
-          span {
-           display: inline-block;
-            font-family: "Handlee", cursive;
-            font-weight: 800;
-            color: #fff;
-            opacity: 0.95;
-          }
-
-          h4 {
-            font-size: 20px;
-            a {
-              pointer-events: none;
-              color: #fff;
-            }
-          }
-        }
-
-        .overlay::after {
-          position: absolute;
-          right: 0;
-          bottom: 0;
-          content: "";
-          height: 100px;
-          width: 100px;
-          background: url(../assets/img/leaf-2.png);
-          background-position-x: 0%;
-          background-position-y: 0%;
-          background-repeat: repeat;
-          background-size: auto;
-          background-repeat: no-repeat;
-          background-position: right bottom;
-          background-size: contain;
-          z-index: -1;
-          opacity: 0.1;
-        }
-      }
-
-      .col-container:hover .overlay {
-        visibility: visible;
-        height: 120px;
-        opacity: 1;
-        box-sizing: border-box;
-        margin-top: opx;
-      }
-    }
+ 
+.box span:nth-child(3)
+{
+  transform:rotate(180deg);
+}
+ 
+.box span:nth-child(4)
+{
+  transform:rotate(270deg);
+}
+ 
+.box span:before
+{
+  content: '';
+  position: absolute;
+  width:100%;
+  height: 2px;
+  background: #50dfdb;
+  animation: animate 30s linear infinite;
+}
+ 
+@keyframes animate {
+  0% {
+  transform:scaleX(0);
+  transform-origin: left;
   }
+  50%
+  {
+    transform:scaleX(1);
+  transform-origin: left;
+  }
+  50.1%
+  {
+    transform:scaleX(1);
+  transform-origin: right;
+    
+  }
+  
+  100%
+  {
+    transform:scaleX(0);
+  transform-origin: right;
+    
+  }
+  
+  
 }
 </style>
-
